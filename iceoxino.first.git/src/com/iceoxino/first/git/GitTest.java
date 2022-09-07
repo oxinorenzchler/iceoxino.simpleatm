82cf66857221857adf3a11d323679ad3205e8998
oxinorenzchler <oxino.renzchler@gmail.com>
2022-09-07 16:39:37 +0800

    Second commit - Printed some text

--- iceoxino.first.git/src/com/iceoxino/first/git/GitTest.java
+++ iceoxino.first.git/src/com/iceoxino/first/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Git integration with Eclipse and Java");
+		System.out.println("This will be the second commit");
 	}
 
 }
